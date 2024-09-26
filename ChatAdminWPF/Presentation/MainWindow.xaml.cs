a3554cc08911669ce6f696d165cb32730e8699cc
David Mads Nielsen <davnie01@easv365.dk>
2024-09-26 10:58:28 +0200

    mapper, og clean struktur.

--- ChatAdminWPF/Presentation/MainWindow.xaml.cs
+++ ChatAdminWPF/Presentation/MainWindow.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ChatAdminWPF
+
+namespace ChatAdminWPF.Presentation
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
